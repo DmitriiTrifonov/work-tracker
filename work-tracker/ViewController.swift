0642e0a20e8a800ab73c693dc369c4f62f5244cf
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2020-03-04 16:37:51 +0300

    [ui] -- added timeLabel

--- work-tracker/ViewController.swift
+++ work-tracker/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    var startTime: Date?
+    var stopTime: Date?
     var timer: Timer?
+    @IBOutlet var timeLabel: UILabel!
     
     @IBAction func startStopButton(_ sender: UIButton) {
+        
         sender.isSelected = !sender.isSelected
         
         if !sender.isSelected {
+            stopTime = Date()
             timer?.invalidate()
         } else {
+            startTime = Date()
             timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (t) in
-                print(Date())
+                let interval = DateInterval(start: self.startTime ?? Date() , end: Date()).duration
+                let formatter = DateComponentsFormatter()
+                formatter.allowedUnits = [.hour, .minute, .second]
+                formatter.unitsStyle = .positional
+                formatter.zeroFormattingBehavior = .pad
+                let str = formatter.string(from: interval)
+                self.timeLabel.text = str
             })
         }
         
